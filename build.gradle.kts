9c41a8aac345aac4531b3a13da0131afe5c18e35
Whisperlyric <1814104304@qq.com>
2026-07-14 18:49:55 +0800

    Try use multi versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,8 @@
 plugins {
-    id("fabric-loom") version "1.10-SNAPSHOT"
+    id("fabric-loom") version "1.10-SNAPSHOT" apply false
     id("java")
 }
 
-val minecraft_version: String by project
-val fabric_loader_version: String by project
-val fabric_api_version: String by project
-
-base {
-    archivesName.set(project.name)
-}
-
-version = project.property("mod_version") as String
-group = project.property("mod_group") as String
-
-repositories {
-    mavenCentral()
-    maven("https://maven.fabricmc.net/")
-}
-
-dependencies {
-    minecraft("com.mojang:minecraft:$minecraft_version")
-    mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:$fabric_loader_version")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabric_api_version")
-}
-
-loom {
-    accessWidenerPath.set(file("src/main/resources/anotherinventorysort.accesswidener"))
-}
-
-tasks.processResources {
-    inputs.property("version", project.version)
-    inputs.property("minecraft_version", minecraft_version)
-    inputs.property("loader_version", fabric_loader_version)
-
-    filesMatching("fabric.mod.json") {
-        expand(
-            "version" to project.version,
-            "minecraft_version" to minecraft_version,
-            "loader_version" to fabric_loader_version
-        )
-    }
-}
-
-tasks.withType<JavaCompile> {
-    options.release.set(21)
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
+tasks.register("buildAll") {
+    dependsOn(subprojects.map { it.tasks.named("build") })
 }
